81f52fb0481e28abee1f36eff6583f025262cd0e
juancjara <juanc.jara@pucp.pe>
2015-03-12 21:16:30 -0500

    ggwp

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var url = require('url');
+
+var app = express(); 
+
+app.get('/cb', function(req, res) {
+  console.log('aca')
+  console.log(req.url);
+  var url_parts = url.parse(req.url,true);
+  var query = url_parts.query;
+  console.log(query);
+  if(query.hasOwnProperty('hub.verify_token')){
+    if(query['hub.verify_token']==="ikickass"){
+      res.send(query['hub.challenge']);
+    }
+  }
+});
+
+var port = 1523;
+console.log(port)
+
+app.listen(port);
